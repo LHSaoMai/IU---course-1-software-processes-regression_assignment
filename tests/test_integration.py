a6535e9ec35592b85dc01834f0a930409678b2c3
LHSaoMai <lhoangsaomai@gmail.com>
2026-02-17 23:40:22 +0100

    new test for 5 years (status Gold) - TDD red

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,14 +1,17 @@
 import pytest 
 from app.store import get_customer_status, calculate_discount
 
-#  UNIT TEST: Checking Module A in isolation
 @pytest.mark.parametrize('years,expected_discounts',
                          [(0,0),
                           (10,10),
-                          (12,10)])
+                          (12,10),
+                          (5,5),
+                          (8,5)]
+                          )
 
 def test_integration_discount(years,expected_discounts):
     status = get_customer_status(years)
     discount = calculate_discount(status)
 
     assert discount == expected_discounts 
+
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -5,7 +5,9 @@ from app.store import get_customer_status, calculate_discount
 @pytest.mark.parametrize('years ,expected_status',
         [(0,'standard'),
         (10,'platinum'),
-        (15,'platinum')]
+        (15,'platinum'),
+        (5,'gold'), # UNIT TEST: new feature
+        (8,'gold')] # UNIT TEST: new feature
 )
 def test_all_customer_status(years,expected_status):
     result = get_customer_status(years)
@@ -15,7 +17,8 @@ def test_all_customer_status(years,expected_status):
 #  UNIT TEST: Checking Module B in isolation
 @pytest.mark.parametrize('status,expected_discount',
         [('standard',0),
-        ('platinum',10)]
+        ('platinum',10),
+        ('gold',5)] # UNIT TEST: new feature
                          )
 def test_all_discount(status,expected_discount):
     discount = calculate_discount(status)
